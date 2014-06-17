a7bfea49b6aeee9158606023d3044d2923e2c249
wangxr <wangxr1990123@126.com>
2014-06-17 11:58:55 +0800

    Initial Commit

---
+++ FreeGameRechange/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FreeGameRechange
+//
+//  Created by wangxr on 14-6-17.
+//  Copyright (c) 2014年 wangxr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
